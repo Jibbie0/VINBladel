2783c720515d37b7554de08cabfab6652d05f511
Jibbie0 <halaswolff07@gmail.com>
2024-01-19 17:08:28 -0600

    Initial Commit

---
+++ VinBladelNew/VinBladelNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinBladelNewApp.swift
+//  VinBladelNew
+//
+//  Created by Halas Wolff on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct VinBladelNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

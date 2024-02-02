0fdb74b56839d4cd9fbf1f37e9b59aef7201b1d1
JamesBalek <jbalek6020@stu.d214.org>
2024-02-02 10:35:33 -0600

    Created the structs for the manual and scan vin pages

---
+++ VinBladelNew/HomeScreen.swift
@@ -0,0 +1,24 @@
+//
+//  HomeScreen.swift
+//  VinBladelNew
+//
+//  Created by James Balek on 2/2/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Button("Scan VIN"){
+            
+        }
+        Button("Manually Input VIN") {
+            
+        }
+        Button("Search By Name") {
+            
+        }
+    }
+}
--- VinBladelNew/frontend/ScanVinPage.swift
+++ VinBladelNew/frontend/ScanVinPage.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ScanVinPage: View {
+    var body: some View {
+        Text("Text")
+    }
+}
+
+#Preview {
+    ScanVinPage()
+}

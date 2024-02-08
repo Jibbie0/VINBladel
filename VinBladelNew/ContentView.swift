0f16d3f0b3f2a458cb1903390a0cd67d625e8d5e
AAlchi <alialchi07@gmail.com>
2024-02-08 11:07:00 -0600

    Final Commit for the Day

--- VinBladelNew/ContentView.swift
+++ VinBladelNew/ContentView.swift
@@ -11,7 +11,7 @@ import Foundation
 struct ContentView: View {
     @ObservedObject var firebaseClass = FirebaseClass()
     var body: some View {
-        HomeScreen()
+        Summary_Page()
     }
 }
 

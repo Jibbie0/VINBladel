72c5582b78d0e314ec6060766edaefce4dc498db
Jibbie0 <halaswolff07@gmail.com>
2024-02-14 11:00:04 -0600

    Added InspectionList Comp

--- VinBladelNew/frontend/General_Inspection.swift
+++ VinBladelNew/frontend/General_Inspection.swift
@@ -9,8 +9,17 @@ import Foundation
 import SwiftUI
 
 struct GeneralView: View {
+    let inspectionList: [String] = ["Wiper Blades", "Head Lights", "Mini Lights", "Air Filter", "Cabin Air Filter", "PCV Value", "Washer Fluid", "Engine Oil", "Power Steering Fluid", "Master Cyl. Fluid Level", "Brake Fluid", "Coolant Hoses", "Engine Coolant"]
     var body: some View {
-        Image("ColorsMeaning")
+        VStack {
+            InspectionList(inspectionList: inspectionList)
+        }
     }
 }
+
+#Preview {
+    GeneralView()
+}
+
+
 

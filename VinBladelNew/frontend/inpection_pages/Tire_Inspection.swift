8605a5d82f1c891d330d1e053056fad2fc8b3dae
JamesBalek <jbalek6020@stu.d214.org>
2024-02-22 10:01:26 -0600

    Page Fixes

---
+++ VinBladelNew/frontend/inpection_pages/Tire_Inspection.swift
@@ -0,0 +1,18 @@
+//
+//  Tire_Inspection.swift
+//  VinBladelNew
+//
+//  Created by Ali AI on 2/22/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct Tire_Inspection: View {
+    var body: some View {
+        Image("ColorsMeaning")
+            .resizable()
+            .frame(width: 700, height: 70)
+    }
+}

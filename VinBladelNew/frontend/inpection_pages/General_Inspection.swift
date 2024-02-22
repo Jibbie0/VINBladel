2c6521611d80fb8cb8758ad7932d3c7f88a77edf
AAlchi <alialchi07@gmail.com>
2024-02-22 10:52:06 -0600

    Commit

--- VinBladelNew/frontend/inpection_pages/General_Inspection.swift
+++ VinBladelNew/frontend/inpection_pages/General_Inspection.swift
@@ -11,12 +11,30 @@ import SwiftUI
 struct General_Inspection: View {
     let inspectionList: [String] = ["Wiper Blades", "Head Lights", "Mini Lights", "Air Filter", "Cabin Air Filter", "PCV Value", "Washer Fluid", "Engine Oil", "Power Steering Fluid", "Master Cyl. Fluid Level", "Brake Fluid", "Coolant Hoses", "Engine Coolant"]
     var body: some View {
-        VStack {
-            InspectionList(inspectionList: inspectionList)
-            Image("ColorsMeaning")
-                .resizable()
-                .frame(width: 500, height: 50)
-                .padding()
+        GeometryReader { geo in
+            VStack() {
+                Text("General Inspection")
+                    .font(.system(size: 27))
+                    .bold()
+                    .padding()
+                ScrollView {
+                    Image("ColorsMeaning")
+                        .resizable()
+                        .frame(width: geo.size.width, height: 50)
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                    InspectionItem(title: "srousppdsihj")
+                }
+            }
         }
     }
 }
